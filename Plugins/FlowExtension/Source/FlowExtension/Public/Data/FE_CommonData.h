b905bfd1e63ed34c303824e0713d70c422bdec80
Variann <kjartandth@gmail.com>
2023-06-23 19:59:07 +0200

    Added structs for better dialogue settings

--- Plugins/FlowExtension/Source/FlowExtension/Public/Data/FE_CommonData.h
+++ Plugins/FlowExtension/Source/FlowExtension/Public/Data/FE_CommonData.h
@@ -6,6 +6,8 @@
 #include "UObject/Object.h"
 #include "FE_CommonData.generated.h"
 
+class UO_DialogueOverrideBase;
+
 USTRUCT(BlueprintType)
 struct FS_FlowPin
 {
@@ -21,4 +23,49 @@ struct FS_FlowPin
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FlowPin")
 	FString PinToolTip;
+};
+
+USTRUCT(BlueprintType)
+struct FS_DialogueSettings
+{
+	GENERATED_BODY()
+
+	/**The text displayed to the player to choose this option.*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialogue Option")
+	FText ButtonText;
+
+	/**The actual dialogue that occurs when the player presses
+	 * the dialogue button.*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialogue Option")
+	FText DialogueText;
+};
+
+USTRUCT(BlueprintType)
+struct FS_DialogueOption
+{
+	GENERATED_BODY()
+	
+	/**The actual dialogue that occurs when the player presses
+	 * the dialogue button.*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialogue Option")
+	FS_DialogueSettings DialogueSettings;
+
+	/**If any of these objects override conditions return true, their dialogue
+	 * settings will be used.*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Instanced, Category = "Dialogue Option")
+	TArray<UO_DialogueOverrideBase*> OptionOverrides;
+};
+
+USTRUCT(BlueprintType)
+struct FS_Script
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialogue Option")
+	FText DialogueText;
+
+	/**If any of these objects override conditions return true, their dialogue
+	 * settings will be used.*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Instanced, Category = "Dialogue Option")
+	TArray<UO_DialogueOverrideBase*> OptionOverrides;
 };
