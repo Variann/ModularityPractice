51457f2404c0cbd4f1958be55bbe9adc03928031
Variann <kjartandth@gmail.com>
2023-06-23 20:23:47 +0200

    Added character data asset

---
+++ Plugins/FlowExtension/Source/FlowExtension/Public/Dialogue/DataAssets/DA_DialogueCharacter.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DA_DialogueCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLOWEXTENSION_API UDA_DialogueCharacter : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(Category = "Visuals", BlueprintReadOnly, EditAnywhere)
+	UTexture2D* CharacterPortrait = nullptr;
+
+	UPROPERTY(Category = "Visuals", BlueprintReadOnly, EditAnywhere)
+	FText Name;
+
+	
+#if WITH_EDITORONLY_DATA
+	
+	UPROPERTY(Category = "Developer Settings", BlueprintReadOnly, EditAnywhere, meta = (DevelopmentOnly))
+	UTexture2D* FlowNodePortrait = nullptr;
+	
+#endif
+	
+};
