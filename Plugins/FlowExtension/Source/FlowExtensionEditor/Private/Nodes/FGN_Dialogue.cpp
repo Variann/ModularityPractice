40221eed6b70d30f7e04e5d225f42ff455f32a4a
Variann <kjartandth@gmail.com>
2023-06-23 13:43:17 +0200

    Added dialogue graph node

---
+++ Plugins/FlowExtension/Source/FlowExtensionEditor/Private/Nodes/FGN_Dialogue.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Nodes/FGN_Dialogue.h"
+#include "Dialogue/FN_DialogueBase.h"
+
+#include "Slate/SFGN_Dialogue.h"
+
+UFGN_Dialogue::UFGN_Dialogue(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	AssignedNodeClasses = {UFN_DialogueBase::StaticClass()};
+}
+
+TSharedPtr<SGraphNode> UFGN_Dialogue::CreateVisualWidget()
+{
+	return SNew(SFGN_Dialogue, this);
+}
---
+++ Plugins/FlowExtension/Source/FlowExtensionEditor/Public/Nodes/FGN_Dialogue.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Graph/Nodes/FlowGraphNode.h"
+
+#include "FGN_Dialogue.generated.h"
+
+
+UCLASS()
+class FLOWEXTENSIONEDITOR_API UFGN_Dialogue : public UFlowGraphNode
+{
+	GENERATED_UCLASS_BODY()
+	
+public:
+	
+	virtual TSharedPtr<SGraphNode> CreateVisualWidget() override;
+};
