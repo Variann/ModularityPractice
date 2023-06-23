c9cce039aa4843a63a90d05ae6bd6dcf1a582e85
Variann <kjartandth@gmail.com>
2023-06-23 13:44:04 +0200

    Added base dialogue node

---
+++ Plugins/FlowExtension/Source/FlowExtension/Private/Dialogue/FN_DialogueBase.cpp
@@ -0,0 +1,77 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Dialogue/FN_DialogueBase.h"
+
+const FString UFN_DialogueBase::Continue = TEXT("Continue");
+
+UFN_DialogueBase::UFN_DialogueBase(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer)
+{
+#if WITH_EDITOR
+	Category = TEXT("Dialogue");
+#endif
+	RefreshOutputs();
+}
+
+void UFN_DialogueBase::ExecuteInput(const FName& PinName)
+{
+	// Show your Dialogue UI here
+}
+
+#if WITH_EDITOR
+void UFN_DialogueBase::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	RefreshOutputs();
+	OnReconstructionRequested.ExecuteIfBound();
+	
+
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+}
+
+void UFN_DialogueBase::FixNode(UEdGraphNode* NewGraph)
+{
+	RefreshOutputs();
+}
+#endif
+
+void UFN_DialogueBase::RefreshOutputs()
+{
+	OutputPins.Empty();
+
+	if (!Answers.Num())
+		OutputPins.Add({Continue});
+
+	for (FFlowPin Answer : Answers)
+	{
+		OutputPins.Add(Answer);
+	}
+}
+
+void UFN_DialogueBase::Stop() const
+{
+}
+
+// TArray<FFlowPin> UFN_DialogueBase::GetCustomOutputPins_Implementation()
+// {
+// 	return OutputPins;
+// }
+
+UTexture2D* UFN_DialogueBase::GetSpeakerPortrait_Implementation()
+{
+	return nullptr;
+}
+
+FText UFN_DialogueBase::GetReadableDialogueString_Implementation()
+{
+	return FText(FText::FromString("No text set"));
+}
+
+FText UFN_DialogueBase::GetSpeakerName_Implementation()
+{
+	return FText(FText::FromString("No Name"));
+}
+
+FLinearColor UFN_DialogueBase::GetSpeakerColor_Implementation()
+{
+	return FLinearColor();
+}
---
+++ Plugins/FlowExtension/Source/FlowExtension/Public/Dialogue/FN_DialogueBase.h
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Data/FE_CommonData.h"
+#include "Nodes/FlowNode.h"
+#include "FN_DialogueBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLOWEXTENSION_API UFN_DialogueBase : public UFlowNode
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere, Category=Dialogue)
+	FText Text;
+	UPROPERTY(EditAnywhere, Category=Dialogue)
+	TArray<FText> Answers;
+
+public:
+	static const FString Continue;
+
+	explicit UFN_DialogueBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	virtual void ExecuteInput(const FName& PinName) override;
+
+#if WITH_EDITOR
+
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+
+	virtual void FixNode(UEdGraphNode* NewGraph) override;
+
+#endif
+
+private:
+	void RefreshOutputs();
+protected:
+	virtual void Stop() const;
+
+
+public:
+
+	UFUNCTION(BlueprintNativeEvent)
+	UTexture2D* GetSpeakerPortrait();
+
+	UFUNCTION(BlueprintNativeEvent)
+	FText GetReadableDialogueString();
+
+	UFUNCTION(BlueprintNativeEvent)
+	FText GetSpeakerName();
+
+	UFUNCTION(BlueprintNativeEvent)
+	FLinearColor GetSpeakerColor();
+
+	// UFUNCTION(BlueprintNativeEvent)
+	// TArray<FS_FlowPin> GetCustomOutputPins();
+};
