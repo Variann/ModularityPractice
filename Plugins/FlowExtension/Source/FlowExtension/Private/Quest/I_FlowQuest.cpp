ca2236ff656badd14d63a0585069296dcf2e06ca
Variann <kjartandth@gmail.com>
2023-06-24 07:03:31 +0200

    Added quest interface

---
+++ Plugins/FlowExtension/Source/FlowExtension/Private/Quest/I_FlowQuest.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Quest/I_FlowQuest.h"
+
+
+// Add default functionality here for any II_FlowQuest functions that are not pure virtual.
