300b17df1983e9f0db890635c77e4a454b5770c4
Variann <kjartandth@gmail.com>
2023-06-24 19:30:23 +0200

    moved QuestBase

---
+++ Plugins/FlowExtension/Source/FlowExtension/Private/Quest/Nodes/FN_QuestBase.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Quest/Nodes/FN_QuestBase.h"
+
+UFN_QuestBase::UFN_QuestBase(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+#if WITH_EDITOR
+	Category = TEXT("Quest");
+#endif
+}
---
+++ Plugins/FlowExtension/Source/FlowExtension/Public/Quest/Nodes/FN_QuestBase.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Nodes/FlowNode.h"
+#include "FN_QuestBase.generated.h"
+
+
+
+UCLASS()
+class FLOWEXTENSION_API UFN_QuestBase : public UFlowNode
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, Category=Dialogue)
+	FGameplayTag Quest;
+
+	UPROPERTY(EditAnywhere, Category=Dialogue)
+	FGameplayTagContainer Tasks;
+
+	UPROPERTY(EditAnywhere, Category=Dialogue)
+	FText QuestText;
+};
