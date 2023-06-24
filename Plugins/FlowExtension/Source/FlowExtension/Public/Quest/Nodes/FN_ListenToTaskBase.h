5213f4ab9a08ea63ca2db30e70bd0c10c364ba60
Variann <kjartandth@gmail.com>
2023-06-24 18:40:34 +0200

    Added ListenToTaskBase node

---
+++ Plugins/FlowExtension/Source/FlowExtension/Public/Quest/Nodes/FN_ListenToTaskBase.h
@@ -0,0 +1,16 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Nodes/FlowNode.h"
+#include "FN_ListenToTaskBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLOWEXTENSION_API UFN_ListenToTaskBase : public UFlowNode
+{
+	GENERATED_BODY()
+};
