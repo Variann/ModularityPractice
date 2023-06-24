eeea22f90675fede38344330320d80805aea48e2
Variann <kjartandth@gmail.com>
2023-06-24 18:41:20 +0200

    Added helper function library for testing

---
+++ Source/ModularityPractice/FL_HelperFunctions.cpp
@@ -0,0 +1,32 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+
+#include "FL_HelperFunctions.h"
+
+UObject* UFL_HelperFunctions::GetObjectsDefaultObject(UObject* Object)
+{
+	if(IsValid(Object))
+	{
+		UObject* TempObject = Object->GetClass()->GetDefaultObject();
+		FString ObjectName = TempObject->GetName();
+		return TempObject;
+	}
+	return nullptr;
+}
+
+TArray<UFlowNode*> UFL_HelperFunctions::GetFlowNodes(UFlowAsset* FlowAsset)
+{
+	// UFlowGraph* NewAsset = NewObject<UFlowGraph>();
+	TArray<UFlowNode*> FoundNodes;
+
+	if(FlowAsset)
+	{
+		FlowAsset->PreloadNodes();
+		const TMap<FGuid, UFlowNode*>& AssetNodes = FlowAsset->GetNodes();
+		AssetNodes.GenerateValueArray(FoundNodes);
+		
+		return FoundNodes;
+	}
+
+	return FoundNodes;
+}
---
+++ Source/ModularityPractice/FL_HelperFunctions.h
@@ -0,0 +1,26 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FlowAsset.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Nodes/FlowNode.h"
+#include "FL_HelperFunctions.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MODULARITYPRACTICE_API UFL_HelperFunctions : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	static UObject* GetObjectsDefaultObject(UObject* Object);
+
+	UFUNCTION(BlueprintCallable)
+	static TArray<UFlowNode*> GetFlowNodes(UFlowAsset* FlowAsset);
+};
