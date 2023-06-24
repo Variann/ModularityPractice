e40c40a353b887176b6e5063d8f4421846ca03c7
Variann <kjartandth@gmail.com>
2023-06-24 18:44:30 +0200

    Removed old class references

--- Plugins/FlowExtension/Source/FlowExtension/Public/Data/FE_CommonData.h
+++ Plugins/FlowExtension/Source/FlowExtension/Public/Data/FE_CommonData.h
@@ -1,11 +1,17 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
 
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "UObject/Object.h"
 #include "FE_CommonData.generated.h"
 
+class UFE_QuestRequirement;
+class UFE_Reward;
+class UWidget;
+class UFE_QuestTask;
+class UFE_Quest;
 class UO_DialogueOverrideBase;
 
 USTRUCT(BlueprintType)
@@ -25,6 +31,10 @@ struct FS_FlowPin
 	FString PinToolTip;
 };
 
+
+//--------------//
+//	 Dialogue	//
+
 USTRUCT(BlueprintType)
 struct FS_DialogueSettings
 {
@@ -38,6 +48,12 @@ struct FS_DialogueSettings
 	 * the dialogue button.*/
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialogue Option")
 	FText DialogueText;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialogue Option")
+	bool Optional;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialogue Option")
+	bool Test;
 };
 
 USTRUCT(BlueprintType)
@@ -69,3 +85,107 @@ struct FS_Script
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Instanced, Category = "Dialogue Option")
 	TArray<UO_DialogueOverrideBase*> OptionOverrides;
 };
+
+
+//----------//
+
+
+//----------//
+//	 Task	//
+
+USTRUCT(BlueprintType)
+struct FS_TaskReward
+{
+	GENERATED_BODY()
+
+	// UPROPERTY(Category = "Reward", EditAnywhere, BlueprintReadOnly)
+	// TSubclassOf<UFE_Reward> Task;
+
+	UPROPERTY(Category = "Reward", EditAnywhere, BlueprintReadOnly)
+	bool bIsAccepted = false;
+};
+
+USTRUCT(BlueprintType)
+struct FS_TaskRequirement
+{
+	GENERATED_BODY()
+
+	// UPROPERTY(Category = "Task", EditAnywhere, BlueprintReadOnly)
+	// TSubclassOf<UFE_> Task;
+
+	UPROPERTY(Category = "Task", EditAnywhere, BlueprintReadOnly)
+	bool bIsCompleted = false;
+};
+
+USTRUCT(BlueprintType)
+struct FS_TaskWrapper
+{
+	GENERATED_BODY()
+
+	// UPROPERTY(Category = "Task", EditAnywhere, BlueprintReadOnly)
+	// TSubclassOf<UFE_QuestTask> Task;
+
+	UPROPERTY(Category = "Task", EditAnywhere, BlueprintReadOnly)
+	TArray<FS_TaskRequirement> Requirements;
+
+	/**What state is the task currently in?*/
+	UPROPERTY(Category = "Task", EditAnywhere, BlueprintReadOnly, meta=(Categories="Flow.Quests.State"))
+	FGameplayTag State;
+
+	/**Arbitrary data which can be added to a task, such as a timer, repeat count, etc.*/
+	UPROPERTY(Category = "Task", EditAnywhere, BlueprintReadOnly, meta=(ForceInlineRow), meta=(Categories="Flow.Quests.Metadata"))
+	TMap<FGameplayTag, float> Metadata;
+
+	/**When the task receives interface updates, these widgets will receive the same
+	 * interface call.*/
+	UPROPERTY(Category = "Task", EditAnywhere, BlueprintReadOnly)
+	TArray<UWidget*> ExternalWidgets;
+};
+
+//----------//
+
+
+//----------//
+//	Quest	//
+
+USTRUCT(BlueprintType)
+struct FS_QuestRequirement
+{
+	GENERATED_BODY()
+
+	// UPROPERTY(Category = "Quest", Instanced, EditAnywhere, BlueprintReadOnly)
+	// UFE_QuestRequirement* QuestRequirement;
+
+	UPROPERTY(Category = "Quest", BlueprintReadOnly)
+	bool bIsCompleted = false;
+};
+
+USTRUCT(BlueprintType)
+struct FS_QuestWrapper
+{
+	GENERATED_BODY()
+
+	// UPROPERTY(Category = "Quest", EditAnywhere, BlueprintReadOnly)
+	// TSubclassOf<UFE_Quest> Quest;
+
+	UPROPERTY(Category = "Quest", EditAnywhere, BlueprintReadOnly)
+	TArray<FS_TaskWrapper> Tasks;
+
+	UPROPERTY(Category = "Quest", EditAnywhere, BlueprintReadOnly)
+	TArray<FS_QuestRequirement> Requirements;
+
+	/**What state is the quest currently in?*/
+	UPROPERTY(Category = "Quest", EditAnywhere, BlueprintReadOnly, meta=(Categories="Flow.Quests.State"))
+	FGameplayTag State;
+
+	/**Arbitrary data which can be added to a quest, such as a timer, repeat count, etc.*/
+	UPROPERTY(Category = "Quest", EditAnywhere, BlueprintReadOnly, meta=(ForceInlineRow), meta=(Categories="Flow.Quests.Metadata"))
+	TMap<FGameplayTag, float> Metadata;
+
+	/**When the quest receives interface updates, these widgets will receive the same
+	 * interface call.*/
+	UPROPERTY(Category = "Quest", EditAnywhere, BlueprintReadOnly)
+	TArray<UWidget*> ExternalWidgets;
+};
+
+//----------//
