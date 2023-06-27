95ce949b566daf027470ec45fae273f972b34791
Variann <kjartandth@gmail.com>
2023-06-27 14:48:36 +0200

    Made interface for quest updates

---
+++ Plugins/FlowExtension/Source/FlowExtension/Private/Quest/I_QuestUpdates.cpp
@@ -0,0 +1,7 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+
+#include "Quest/I_QuestUpdates.h"
+
+
+// Add default functionality here for any II_QuestUpdates functions that are not pure virtual.
---
+++ Plugins/FlowExtension/Source/FlowExtension/Public/Quest/I_QuestUpdates.h
@@ -0,0 +1,35 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Data/FE_CommonData.h"
+#include "UObject/Interface.h"
+#include "I_QuestUpdates.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UI_QuestUpdates : public UInterface
+{
+	GENERATED_BODY()
+};
+
+
+/**Quests and tasks can have "listeners". When the quest or task
+ * is updated in any way, the listeners should be notified via
+ * this interface.
+ */
+
+class FLOWEXTENSION_API II_QuestUpdates
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+
+	UFUNCTION(Category = "Quest System", BlueprintNativeEvent, BlueprintCallable)
+	void QuestStateUpdated(FS_QuestWrapper Quest, TEnumAsByte<EQuestState> NewState);
+
+	UFUNCTION(Category = "Quest System", BlueprintNativeEvent, BlueprintCallable)
+	void TaskProgressed(FS_TaskWrapper Task, float ProgressMade, UObject* Instigator);
+};
