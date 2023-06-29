62eb235f2f4925fdd13588e9ba2ba935e78417b5
Variann <kjartandth@gmail.com>
2023-06-29 22:24:53 +0200

    Added WrapQuest and WrapTask

--- Plugins/FlowExtension/Source/FlowExtension/Private/Quest/FL_QuestHelpers.cpp
+++ Plugins/FlowExtension/Source/FlowExtension/Private/Quest/FL_QuestHelpers.cpp
@@ -45,3 +45,39 @@ TArray<UFlowNode*> UFL_QuestHelpers::GetFlowNodes(UFlowAsset* FlowAsset)
 
 	return FoundNodes;
 }
+
+FS_QuestWrapper UFL_QuestHelpers::WrapQuest(FS_Quest QuestInformation, UFN_QuestBase* QuestNode)
+{
+	FS_QuestWrapper QuestWrapper;
+
+	if(IsValid(QuestNode))
+	{
+		QuestWrapper.Graph = QuestNode->GetFlowAsset();
+		QuestWrapper.ParentNode = QuestNode;
+	}
+	
+	QuestWrapper.QuestID = QuestInformation.QuestID;
+	QuestWrapper.QuestName = QuestInformation.QuestName;
+	QuestWrapper.Requirements = QuestInformation.Requirements;
+	QuestWrapper.FailConditions = QuestInformation.FailConditions;
+	QuestWrapper.Rewards = QuestInformation.Rewards;
+	QuestWrapper.State = InProgress;
+
+	return QuestWrapper;
+}
+
+FS_TaskWrapper UFL_QuestHelpers::WrapTask(FS_QuestTask TaskInformation)
+{
+	FS_TaskWrapper TaskWrapper;
+	
+	TaskWrapper.TaskID = TaskInformation.TaskID;
+	TaskWrapper.TaskName = TaskInformation.TaskName;
+	TaskWrapper.Requirements = TaskInformation.Requirements;
+	TaskWrapper.FailConditions = TaskInformation.FailConditions;
+	TaskWrapper.Rewards = TaskInformation.Rewards;
+	TaskWrapper.State = InProgress;
+	TaskWrapper.ProgressRequired = TaskInformation.ProgressRequired;
+	TaskWrapper.IsOptional = TaskInformation.IsOptional;
+
+	return TaskWrapper;
+}
--- Plugins/FlowExtension/Source/FlowExtension/Public/Quest/FL_QuestHelpers.h
+++ Plugins/FlowExtension/Source/FlowExtension/Public/Quest/FL_QuestHelpers.h
@@ -3,10 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Data/FE_CommonData.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "Nodes/FlowNode.h"
 #include "FL_QuestHelpers.generated.h"
 
+class UFN_QuestBase;
 /**
  * 
  */
@@ -15,9 +17,20 @@ class FLOWEXTENSION_API UFL_QuestHelpers : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 
+public:
+
 	UFUNCTION(BlueprintCallable)
 	static UFlowNode* GetQuestNode(UFlowAsset* Graph, FGameplayTag QuestID);
 
 	UFUNCTION(BlueprintCallable)
 	static TArray<UFlowNode*> GetFlowNodes(UFlowAsset* FlowAsset);
+
+	/**Wrap a quest into the struct the quest component will use.
+	 *
+	 * @QuestNode This is optional.*/
+	UFUNCTION(BlueprintCallable)
+	static FS_QuestWrapper WrapQuest(FS_Quest QuestInformation, UFN_QuestBase* QuestNode);
+
+	UFUNCTION(BlueprintCallable)
+	static FS_TaskWrapper WrapTask(FS_QuestTask TaskInformation);
 };
