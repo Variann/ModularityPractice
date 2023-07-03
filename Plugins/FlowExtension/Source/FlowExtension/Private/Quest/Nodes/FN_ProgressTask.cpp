3040d6b76b5241d6e45dc6390a51b275177c4aab
Variann <kjartandth@gmail.com>
2023-07-03 19:11:15 +0200

    Added ProgressTask node

---
+++ Plugins/FlowExtension/Source/FlowExtension/Private/Quest/Nodes/FN_ProgressTask.cpp
@@ -0,0 +1,45 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+
+#include "Quest/Nodes/FN_ProgressTask.h"
+
+UFN_ProgressTask::UFN_ProgressTask(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+#if WITH_EDITOR
+	Category = TEXT("Quest");
+#endif
+}
+
+FString UFN_ProgressTask::GetNodeDescription() const
+{
+	if(Task.IsValid())
+	{
+		return Task.ToString();
+	}
+	
+	return Super::GetNodeDescription();
+}
+
+EDataValidationResult UFN_ProgressTask::ValidateNode()
+{
+	bool FailedValidation = false;
+	
+	if(!Task.IsValid())
+	{
+		ValidationLog.Error<UFlowNode>(TEXT("Missing Task"), this);
+		FailedValidation = true;
+	}
+
+	if(Progress == 0)
+	{
+		ValidationLog.Error<UFlowNode>(TEXT("Missing Task"), this);
+		FailedValidation = true;
+	}
+
+	if(FailedValidation)
+	{
+		return EDataValidationResult::Invalid;
+	}
+	return EDataValidationResult::Valid;
+}
---
+++ Plugins/FlowExtension/Source/FlowExtension/Public/Quest/Nodes/FN_ProgressTask.h
@@ -0,0 +1,28 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Nodes/FlowNode.h"
+#include "FN_ProgressTask.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class FLOWEXTENSION_API UFN_ProgressTask : public UFlowNode
+{
+	GENERATED_UCLASS_BODY()
+
+	UPROPERTY(Category = "Task", EditAnywhere, BlueprintReadOnly, meta=(Categories="Flow.Quests"))
+	FGameplayTag Task;
+
+	UPROPERTY(Category = "Task", EditAnywhere, BlueprintReadOnly)
+	float Progress = 0;
+
+
+#if WITH_EDITOR
+	virtual FString GetNodeDescription() const override;
+	virtual EDataValidationResult ValidateNode() override;
+#endif
+};
