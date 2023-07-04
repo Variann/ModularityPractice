7637ffee23fda8b9e6c4154caee24f1697e24ff2
Variann <kjartandth@gmail.com>
2023-07-04 22:44:30 +0200

    Added DialogueCondition class

---
+++ Plugins/FlowExtension/Source/FlowExtension/Private/Dialogue/Objects/O_DialogueConditionBase.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+
+#include "Dialogue/Objects/O_DialogueConditionBase.h"
+
+bool UO_DialogueConditionBase::IsConditionMet_Implementation()
+{
+	return true;
+}
---
+++ Plugins/FlowExtension/Source/FlowExtension/Public/Dialogue/Objects/O_DialogueConditionBase.h
@@ -0,0 +1,31 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FlowAsset.h"
+#include "UObject/Object.h"
+#include "O_DialogueConditionBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, Blueprintable, BlueprintType, meta=(ShowWorldContextPin), AutoExpandCategories = ("Default"), EditInlineNew)
+class FLOWEXTENSION_API UO_DialogueConditionBase : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(Category = "Dialogue", EditAnywhere, BlueprintReadOnly)
+	UFlowAsset* FlowGraph = nullptr;
+
+	/**If the owning dialogue option does NOT want to be hidden, but the requirements
+	 * are still not met, the option will be greyed out and not selectable, but still
+	 * visible to the player.*/
+	UPROPERTY(Category = "Dialogue", EditAnywhere, BlueprintReadOnly)
+	bool HideIfConditionIsNotMet = true;
+
+	UFUNCTION(Category = "Dialogue", BlueprintCallable, BlueprintPure, BlueprintNativeEvent)
+	bool IsConditionMet();
+};
