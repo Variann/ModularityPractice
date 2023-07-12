b702f203e0c3dc29e9bdce1be92c4461cfa00b04
Variann <kjartandth@gmail.com>
2023-07-12 11:07:48 +0200

    Add RelationsSubSystem files to Relations plugin
    New 'RelationsSubSystem' class files are added to the Relations plugin. These files will be used to handle any operations related to relationships between different entities in the game. The files include a header (.h) and source (.cpp) code file for the RelationsSubSystem class.

---
+++ Plugins/Relations/Source/Relations/Public/Core/RelationsSubSystem.h
@@ -0,0 +1,17 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "RelationsSubSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RELATIONS_API URelationsSubSystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
