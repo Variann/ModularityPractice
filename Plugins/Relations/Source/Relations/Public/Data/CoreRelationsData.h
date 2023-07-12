816c2df9a695d74f8067b9c18b46bf0550421b5b
Variann <kjartandth@gmail.com>
2023-07-12 11:08:39 +0200

    Add CoreRelationsData header for relations data handling
    A new file `CoreRelationsData.h` has been added to define structures related to character relationships in the gameplay. This includes `FS_RelationStatus`, `FS_RelationshipsDataRow`, and `FS_Relationship`. These structures will facilitate handling of relations parameters, status, and experience levels - crucial in gameplay logic and dynamics.

---
+++ Plugins/Relations/Source/Relations/Public/Data/CoreRelationsData.h
@@ -0,0 +1,58 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "AttributeSet.h"
+#include "ScalableFloat.h"
+#include "CoreRelationsData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FS_RelationStatus
+{
+	GENERATED_BODY()
+
+	UPROPERTY(Category = "Relation", EditAnywhere, BlueprintReadWrite)
+	FText StatusText;
+
+	UPROPERTY(Category = "Relation", EditAnywhere, BlueprintReadWrite)
+	FGameplayTag StatusID;
+
+	UPROPERTY(Category = "Relation", EditAnywhere, BlueprintReadWrite)
+	float MinLevel;
+
+	UPROPERTY(Category = "Relation", EditAnywhere, BlueprintReadWrite)
+	float MaxLevel;
+};
+
+USTRUCT(BlueprintType)
+struct FS_RelationshipsDataRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(Category = "Relation", EditAnywhere, BlueprintReadWrite)
+	FGameplayTag Entity;
+
+	UPROPERTY(Category = "Relation", EditAnywhere, BlueprintReadWrite)
+	FText EntityName;
+
+	UPROPERTY(Category = "Relation", EditAnywhere, BlueprintReadWrite)
+	FRuntimeFloatCurve ExperienceAndLevelCurve;
+
+	UPROPERTY(Category = "Relation", EditAnywhere, BlueprintReadWrite, meta = (TitleProperty = "StatusText"))
+	TArray<FS_RelationStatus> RelationStatuses;
+};
+
+USTRUCT(BlueprintType)
+struct FS_Relationship
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(Category = "Relation", EditAnywhere, BlueprintReadWrite)
+	FGameplayTag Entity;
+
+	UPROPERTY(Category = "Relation", EditAnywhere, BlueprintReadWrite)
+	float CurrentXP;
+
+	UPROPERTY(Category = "Relation", EditAnywhere, BlueprintReadWrite)
+	FS_RelationStatus CurrentRelationStatus;
+};
