3cae641183124e6589dbe8dd206122b8143f56eb
Variann <kjartandth@gmail.com>
2023-07-12 11:08:23 +0200

    Add RelationHelper functions for ScalableFloat and RuntimeFloatCurve
    This commit introduces a new RelationHelpers module to process ScalableFloat values and RuntimeFloatCurve keys. It provides two functions: GetValueAtLevel to fetch the value of a ScalableFloat at a specific level and GetRuntimeCurveKeys to retrieve all keys from a given RuntimeFloatCurve. These functions were created to assist in data handling within the Relations Plugin, ensuring more efficient and streamlined operations.

---
+++ Plugins/Relations/Source/Relations/Private/Data/FL_RelationHelpers.cpp
@@ -0,0 +1,27 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+
+#include "Data/FL_RelationHelpers.h"
+
+float UFL_RelationHelpers::GetValueAtLevel(FScalableFloat ScalableFloat, float Level, FString ContextString)
+{
+	return ScalableFloat.GetValueAtLevel(Level);
+}
+
+TMap<float, float> UFL_RelationHelpers::GetRuntimeCurveKeys(FRuntimeFloatCurve RuntimeCurveFloat)
+{
+	TMap<float, float> FoundLevels;
+	FRuntimeFloatCurve Levels = RuntimeCurveFloat;
+
+	if(!RuntimeCurveFloat.GetRichCurve())
+	{
+		return FoundLevels;
+	}
+	
+	for(auto& CurrentKey : Levels.GetRichCurve()->Keys)
+	{
+		FoundLevels.Add(CurrentKey.Time, CurrentKey.Value);
+	}
+
+	return FoundLevels;
+}
---
+++ Plugins/Relations/Source/Relations/Public/Data/FL_RelationHelpers.h
@@ -0,0 +1,24 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CoreRelationsData.h"
+#include "ScalableFloat.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "FL_RelationHelpers.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RELATIONS_API UFL_RelationHelpers : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	UFUNCTION(BlueprintCallable)
+	static float GetValueAtLevel(FScalableFloat ScalableFloat, float Level, FString ContextString);
+
+	UFUNCTION(BlueprintCallable)
+	static TMap<float, float> GetRuntimeCurveKeys(FRuntimeFloatCurve RuntimeCurveFloat);
+};
