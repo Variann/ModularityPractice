bded30880c81739f534ab006a6c92090cc8013aa
Variann <kjartandth@gmail.com>
2023-08-02 21:35:34 +0200

    Added LayeredUI editor module

---
+++ Plugins/LayeredUI/Source/LayeredUIEditor/LayeredUIEditor.Build.cs
@@ -0,0 +1,29 @@
+﻿using UnrealBuildTool;
+
+public class LayeredUIEditor : ModuleRules
+{
+    public LayeredUIEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "PropertyEditor",
+                "Blutility", 
+                "LayeredUI"
+            }
+        );
+    }
+}
---
+++ Plugins/LayeredUI/Source/LayeredUIEditor/Private/LayeredUIEditor.cpp
@@ -0,0 +1,28 @@
+﻿#include "LayeredUIEditor.h"
+
+#include "FWidgetLayerCustomization.h"
+#include "PropertyEditorModule.h"
+#include "Data/CoreLayeredUIData.h"
+
+IMPLEMENT_GAME_MODULE(FLayeredUIEditorModule, TagFactsEditor)
+
+#define LOCTEXT_NAMESPACE "FLayeredUIEditorModule"
+
+void FLayeredUIEditorModule::StartupModule()
+{
+	FPropertyEditorModule& PropertyModule = FModuleManager::LoadModuleChecked<FPropertyEditorModule>("PropertyEditor");
+
+	//Register the FS_Fact struct customization
+	PropertyModule.RegisterCustomPropertyTypeLayout(
+		FWidgetLayer::StaticStruct()->GetFName(),
+		FOnGetPropertyTypeCustomizationInstance::CreateStatic(&FWidgetLayerCustomization::MakeInstance));
+
+	PropertyModule.NotifyCustomizationModuleChanged();
+}
+
+void FLayeredUIEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
