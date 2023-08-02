2f01f63bb2a3f44ee215fcdd89bd60c08a26493b
Variann <kjartandth@gmail.com>
2023-08-02 18:55:29 +0200

    Update LayeredUI loading phase and add subsystem
    The loading phase of the LayeredUI plugin was changed from 'Default' to 'PreDefault' to ensure its loading priority. A LayeredUI_Subsystem was also added - this forms part of our effort to improve UI management by providing another layer of control over the UI system, as well as facilitate gameplay tagging. The subsystem includes a 'GetLayerMap' function which retrieves the gameplay layer map from the corresponding LayeredUIGameSettings.

---
+++ Plugins/LayeredUI/Source/LayeredUI/Private/Core/LayeredUI_Subsystem.cpp
@@ -0,0 +1,23 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+
+#include "Core/LayeredUI_Subsystem.h"
+
+#include <Config/DS_LayeredUIGameSettings.h>
+
+bool ULayeredUI_Subsystem::ShouldCreateSubsystem(UObject* Outer) const
+{
+	return true;
+}
+
+TMap<FGameplayTag, int32> ULayeredUI_Subsystem::GetLayerMap()
+{
+	const UDS_LayeredUIGameSettings* UIData = GetDefault<UDS_LayeredUIGameSettings>();
+
+	if(UIData)
+	{
+		return UIData->LayerMap;
+	}
+
+	return TMap<FGameplayTag, int32>();
+}
---
+++ Plugins/LayeredUI/Source/LayeredUI/Public/Core/LayeredUI_Subsystem.h
@@ -0,0 +1,22 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Subsystems/LocalPlayerSubsystem.h"
+#include "LayeredUI_Subsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAYEREDUI_API ULayeredUI_Subsystem : public ULocalPlayerSubsystem
+{
+	GENERATED_BODY()
+
+	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+
+	UFUNCTION(Category="LayeredUI", BlueprintCallable)
+	TMap<FGameplayTag, int32> GetLayerMap();
+};
