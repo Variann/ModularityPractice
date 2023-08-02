d4493359fd3134b705f5671b575f59ba1e522551
Variann <kjartandth@gmail.com>
2023-08-02 17:57:13 +0200

    Added LayeredUI data asset

---
+++ Plugins/LayeredUI/Source/LayeredUI/Public/Core/DA_SettingsLayeredUI.h
@@ -0,0 +1,16 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DA_SettingsLayeredUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAYEREDUI_API UDA_SettingsLayeredUI : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+};
