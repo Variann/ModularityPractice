5431ce1ff6f0e2392f8f935302d8dc42b0b450f0
Variann <kjartandth@gmail.com>
2023-08-02 22:10:21 +0200

    Add widget layering functionality to LayeredUI subsystem
    This commit expands the LayeredUI subsystem functionality by introducing the ability to manipulate widgets on layers. The diff includes additions of multiple functions, including ones to add/remove a widget from a layer, get current widgets, get layered widgets, and find first widget on a layer. These changes aim to provide developers more control over the UI layering behavior.

--- Plugins/LayeredUI/Source/LayeredUI/Private/Core/LayeredUI_Subsystem.cpp
+++ Plugins/LayeredUI/Source/LayeredUI/Private/Core/LayeredUI_Subsystem.cpp
@@ -5,6 +5,8 @@
 
 #include <Config/DS_LayeredUIGameSettings.h>
 
+#include "Blueprint/UserWidget.h"
+
 bool ULayeredUI_Subsystem::ShouldCreateSubsystem(UObject* Outer) const
 {
 	return true;
@@ -12,12 +14,56 @@ bool ULayeredUI_Subsystem::ShouldCreateSubsystem(UObject* Outer) const
 
 TMap<FGameplayTag, int32> ULayeredUI_Subsystem::GetLayerMap()
 {
-	const UDS_LayeredUIGameSettings* UIData = GetDefault<UDS_LayeredUIGameSettings>();
-
-	if(UIData)
+	if(const UDS_LayeredUIGameSettings* UIData = GetDefault<UDS_LayeredUIGameSettings>())
 	{
 		return UIData->LayerMap;
 	}
 
 	return TMap<FGameplayTag, int32>();
+}
+
+TArray<UUserWidget*> ULayeredUI_Subsystem::GetCurrentWidgets()
+{
+	TArray<UUserWidget*> CurrentWidgets;
+	
+	for(auto& CurrentLayer : LayeredWidgets)
+	{
+		if(IsValid(CurrentLayer.Widget))
+		{
+			CurrentWidgets.Add(CurrentLayer.Widget);
+		}
+	}
+	
+	return CurrentWidgets;
+}
+
+TArray<FLayeredWidget> ULayeredUI_Subsystem::GetLayeredWidgets()
+{
+	return LayeredWidgets;
+}
+
+void ULayeredUI_Subsystem::AddWidgetToLayer(UUserWidget* Widget, FGameplayTag Layer, FLayeredWidget& LayeredWidget,
+                                            int32 OrderOverride)
+{
+}
+
+void ULayeredUI_Subsystem::RemoveWidgetFromLayer(FLayeredWidget Widget, FLayeredWidget& NewFocus)
+{
+}
+
+void ULayeredUI_Subsystem::FindFirstWidgetOnLayer(FGameplayTag Layer, FLayeredWidget& Widget)
+{
+	FLayeredWidget HigherPriorityWidget;
+	HigherPriorityWidget.LayerPriority = -1;
+	for(auto& CurrentLayer : LayeredWidgets)
+	{
+		if(CurrentLayer.Layer == Layer)
+		{
+			if(CurrentLayer.LayerPriority > HigherPriorityWidget.LayerPriority)
+			{
+				HigherPriorityWidget = CurrentLayer;
+				Widget = HigherPriorityWidget;
+			}
+		}
+	}
 }
--- Plugins/LayeredUI/Source/LayeredUI/Public/Core/LayeredUI_Subsystem.h
+++ Plugins/LayeredUI/Source/LayeredUI/Public/Core/LayeredUI_Subsystem.h
@@ -4,19 +4,45 @@
 
 #include "CoreMinimal.h"
 #include "GameplayTagContainer.h"
+#include "Components/Widget.h"
+#include "Data/CoreLayeredUIData.h"
 #include "Subsystems/LocalPlayerSubsystem.h"
 #include "LayeredUI_Subsystem.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class LAYEREDUI_API ULayeredUI_Subsystem : public ULocalPlayerSubsystem
 {
 	GENERATED_BODY()
 
+private:
+
+	/**References to current widgets that have been added to the screen
+	 * using this subsystem.*/
+	UPROPERTY()
+	TArray<FLayeredWidget> LayeredWidgets;
+
+public: 
+
 	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
 
+	/**Get a copy of the layer map from the project settings.*/
 	UFUNCTION(Category="LayeredUI", BlueprintCallable)
 	TMap<FGameplayTag, int32> GetLayerMap();
+
+	/**Get the references to current widgets that have been added to the screen
+	 * using this subsystem.*/
+	UFUNCTION(Category="LayeredUI", BlueprintCallable)
+	TArray<UUserWidget*> GetCurrentWidgets();
+
+	UFUNCTION(Category="LayeredUI", BlueprintCallable)
+	TArray<FLayeredWidget> GetLayeredWidgets();
+
+	UFUNCTION(Category="LayeredUI", BlueprintCallable)
+	void AddWidgetToLayer(UUserWidget* Widget, FGameplayTag Layer, FLayeredWidget& LayeredWidget, int32 OrderOverride = -1);
+
+	UFUNCTION(Category="LayeredUI", BlueprintCallable)
+	void RemoveWidgetFromLayer(FLayeredWidget Widget, FLayeredWidget& NewFocus);
+	
+	UFUNCTION(Category="LayeredUI", BlueprintCallable)
+	void FindFirstWidgetOnLayer(FGameplayTag Layer, FLayeredWidget& Widget);
 };
