e0e7fbceafbf54056fd58908aceb0eb256e166ad
Variann <kjartandth@gmail.com>
2023-08-03 18:25:26 +0200

    Added interface for widget communication

---
+++ Plugins/LayeredUI/Source/LayeredUI/Private/Data/I_WidgetCommunication.cpp
@@ -0,0 +1,7 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+
+#include "Data/I_WidgetCommunication.h"
+
+
+// Add default functionality here for any II_WidgetCommunication functions that are not pure virtual.
---
+++ Plugins/LayeredUI/Source/LayeredUI/Public/Data/I_WidgetCommunication.h
@@ -0,0 +1,52 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CoreLayeredUIData.h"
+#include "UObject/Interface.h"
+#include "I_WidgetCommunication.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UI_WidgetCommunication : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class LAYEREDUI_API II_WidgetCommunication
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "LayeredUI||Widgets||Cursor")
+	bool HideCursor();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "LayeredUI||Widgets||Layering")
+	void SetWidgetLayerData(FLayeredWidget WidgetLayer);
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "LayeredUI||Widgets||Layering")
+	FLayeredWidget GetWidgetLayerData();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "LayeredUI||Widgets||Settings")
+	bool AllowMultipleInstances();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "LayeredUI||Widgets||Settings")
+	void UpdateWidget();
+
+	//-------------------------
+	/*Some widgets are not part of the CommonUI framework for various reasons, these two interface
+	 events are meant to communicate what state a widget is in or set its state.*/
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "LayeredUI||Widgets||State")
+	bool DeactivateWidgetsState();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "LayeredUI||Widgets||State")
+	bool ActivateWidgetsState();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "LayeredUI||Widgets||State")
+	bool IsWidgetStateActive();
+	
+	//-------------------------
+};
