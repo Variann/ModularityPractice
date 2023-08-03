6356eb669b1d9c21364432e6ad45692fe2db1c2b
Variann <kjartandth@gmail.com>
2023-08-03 18:26:51 +0200

    Added async AddWidgetToLayer

---
+++ Plugins/LayeredUI/Source/LayeredUI/Private/Core/LayeredUI_AsyncFunctions.cpp
@@ -0,0 +1,55 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+
+#include "Core/LayeredUI_AsyncFunctions.h"
+
+#include "Blueprint/UserWidget.h"
+#include "Core/LayeredUI_Subsystem.h"
+#include "Blueprint/UserWidget.h"
+#include "Kismet/GameplayStatics.h"
+
+ULayeredUI_AddWidgetToLayer* ULayeredUI_AddWidgetToLayer::AddWidgetToLayer_Async(TSoftClassPtr<UUserWidget> Widget,
+                                                                                 FGameplayTag Layer, UObject* Context, int32 OrderOverride)
+{
+	ULayeredUI_AddWidgetToLayer* NewAsyncObject = NewObject<ULayeredUI_AddWidgetToLayer>(Context);
+	NewAsyncObject->WidgetToLoad = Widget;
+	NewAsyncObject->LayerDestination = Layer;
+	NewAsyncObject->DesiredOrderOverride = OrderOverride;
+	NewAsyncObject->WidgetOwner = Context;
+	return NewAsyncObject;
+}
+
+void ULayeredUI_AddWidgetToLayer::Activate()
+{
+	Super::Activate();
+
+	Handle = StreamableManager.RequestAsyncLoad(WidgetToLoad.ToSoftObjectPath(), [this]()
+	{
+		FLayeredWidget NewLayeredWidget;
+		LoadedWidget = Cast<UUserWidget>(Handle->GetLoadedAsset());
+		if(LoadedWidget)
+		{
+			UUserWidget* CreatedWidget = Cast<UUserWidget>(CreateWidget(WidgetOwner, LoadedWidget->GetClass()));
+
+			ULayeredUI_Subsystem* LayeredUI_Subsystem = UGameplayStatics::GetPlayerController(WidgetOwner, 0)->GetLocalPlayer()->GetSubsystem<ULayeredUI_Subsystem>();
+			if(!LayeredUI_Subsystem)
+			{
+				Fail.Broadcast(NewLayeredWidget);
+				RemoveFromRoot();
+				return;
+			}
+			
+			LayeredUI_Subsystem->AddWidgetToLayer(CreatedWidget ,LayerDestination, NewLayeredWidget, DesiredOrderOverride);
+
+			if(!IsValid(NewLayeredWidget.Widget))
+			{
+				Fail.Broadcast(NewLayeredWidget);
+				RemoveFromRoot();
+				return;
+			}
+
+			Success.Broadcast(NewLayeredWidget);
+			RemoveFromRoot();
+		}
+	});
+}
---
+++ Plugins/LayeredUI/Source/LayeredUI/Public/Core/LayeredUI_AsyncFunctions.h
@@ -0,0 +1,53 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StreamableManager.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "GameplayTagContainer.h"
+#include "Blueprint/UserWidget.h"
+#include "Data/CoreLayeredUIData.h"
+#include "LayeredUI_AsyncFunctions.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAddWidgetSuccess, FLayeredWidget, NewWidget);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAddWidgetFailed, FLayeredWidget, NewWidget);
+
+
+UCLASS()
+class LAYEREDUI_API ULayeredUI_AddWidgetToLayer : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintAssignable)
+	FAddWidgetSuccess Success;
+
+	UPROPERTY(BlueprintAssignable)
+	FAddWidgetFailed Fail;
+
+	UPROPERTY()
+	TSoftClassPtr<UUserWidget> WidgetToLoad;
+	UPROPERTY()
+	UUserWidget* LoadedWidget;
+	
+	//Needed for loading soft reference.
+	FStreamableManager StreamableManager;
+	TSharedPtr<FStreamableHandle> Handle;
+
+	UPROPERTY()
+	FGameplayTag LayerDestination;
+
+	UPROPERTY()
+	int32 DesiredOrderOverride;
+
+	UPROPERTY()
+	UObject* WidgetOwner;
+
+public:
+
+	UFUNCTION(Category="LayeredUI", BlueprintCallable, meta=(BlueprintInternalUseOnly="true", WorldContext="Context"), DisplayName = "Add widget to Layer (Async)")
+	static ULayeredUI_AddWidgetToLayer* AddWidgetToLayer_Async(TSoftClassPtr<UUserWidget> Widget, FGameplayTag Layer, UObject* Context, int32 OrderOverride = -1);
+
+	virtual void Activate() override;
+};
