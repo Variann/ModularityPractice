dcd707d44ce5db40369ab75e06eb9fb1152b1262
Variann <kjartandth@gmail.com>
2023-08-10 08:23:36 +0200

    Refactor EConditionHandling enum and remove FS_FlowPin struct

--- Plugins/FlowExtension/Source/FlowExtension/Public/Data/FE_CommonData.h
+++ Plugins/FlowExtension/Source/FlowExtension/Public/Data/FE_CommonData.h
@@ -27,26 +27,9 @@ enum EQuestState
 UENUM(BlueprintType)
 enum EConditionHandling
 {
-	SingularCondition,
+	AnyCondition,
 	AllConditions
 };
-
-// USTRUCT(BlueprintType)
-// struct FS_FlowPin
-// {
-// 	GENERATED_BODY()
-//
-// 	// A logical name, used during execution of pin
-// 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FlowPin")
-// 	FName PinName;
-//
-// 	// An optional Display Name, you can use it to override PinName without the need to update graph connections
-// 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FlowPin")
-// 	FText PinFriendlyName;
-//
-// 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FlowPin")
-// 	FString PinToolTip;
-// };
 
 
 //--------------//
