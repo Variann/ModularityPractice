cb4b8b221e7730452b89c5a96cfaeaf708f9cb21
Variann <kjartandth@gmail.com>
2023-08-15 20:30:27 +0200

    Added flow node RandomOutputWithWeights

---
+++ Plugins/FlowExtension/Source/FlowExtension/Private/Other/FN_RandomOutputWithWeights.cpp
@@ -0,0 +1,123 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+
+#include "Other/FN_RandomOutputWithWeights.h"
+
+#include "Kismet/KismetMathLibrary.h"
+
+UFN_RandomOutputWithWeights::UFN_RandomOutputWithWeights(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+#if WITH_EDITOR
+	Category = "Route";
+	RefreshOutputs();
+#endif
+
+	InputPins.Add(FFlowPin(TEXT("Reset Weights")));
+}
+
+FRandomOutputWithWeight UFN_RandomOutputWithWeights::ExecuteRandomOutput()
+{
+	float OutputsTotalSum = 0;
+	for(auto& CurrentOutput : Outputs)
+	{
+		if(Outputs.Num() >= 3 && CurrentOutput.OutputPinName == LastOutput.OutputPinName)
+		{
+			continue;
+		}
+		
+		OutputsTotalSum += CurrentOutput.CurrentWeight;
+	}
+
+	FRandomOutputWithWeight FinalOutput;
+	float RandomValue = UKismetMathLibrary::RandomFloatInRange(0, OutputsTotalSum);
+	float CurrentWeight = 0;
+
+	//Resolve the RNG and weights
+	for(auto& CurrentOutput : Outputs)
+	{
+		if(Outputs.Num() >= 3 && CurrentOutput.OutputPinName == LastOutput.OutputPinName)
+		{
+			CurrentWeight += CurrentOutput.CurrentWeight;
+			continue;
+		}
+		
+		if(RandomValue <= CurrentOutput.CurrentWeight)
+		{
+			TriggerOutput(CurrentOutput.OutputPinName, true);
+			CurrentOutput.CurrentWeight = UKismetMathLibrary::Clamp(CurrentOutput.CurrentWeight - CurrentOutput.WeightLostWhenPicked, 0, CurrentOutput.CurrentWeight);
+			FinalOutput = CurrentOutput;
+			break;
+		}
+		else
+		{
+			RandomValue -= CurrentOutput.CurrentWeight;
+		}
+	}
+
+	//Add up the GainedPerRoll
+	for(auto& CurrentOutput : Outputs)
+	{
+		if(CurrentOutput.OutputPinName != FinalOutput.OutputPinName)
+		{
+			CurrentOutput.CurrentWeight += CurrentOutput.WeightGainedPerRoll;
+		}
+	}
+
+	if(Outputs.Num() >= 3)
+	{
+		LastOutput = FinalOutput;
+	}
+
+	return FinalOutput;
+}
+
+void UFN_RandomOutputWithWeights::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	RefreshOutputs();
+
+	//Tiny runtime optimization, this will pre-set the CurrentWeight
+	//to the base weight so it doesn't have to be set during runtime.
+	for(auto& CurrentOutput : Outputs)
+	{
+		CurrentOutput.CurrentWeight = CurrentOutput.BaseWeight;
+	}
+	
+	OnReconstructionRequested.ExecuteIfBound();
+	
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+}
+
+void UFN_RandomOutputWithWeights::RefreshOutputs()
+{
+	OutputPins.Empty();
+
+	// UpdateCustomOutputPins();
+
+	if (!Outputs.Num())
+		OutputPins.Add(TEXT("Continue"));
+
+	for (auto& OutputPin : Outputs)
+	{
+		OutputPins.Add(FFlowPin(OutputPin.OutputPinName.ToString(), FText(), FString()));
+	}
+}
+
+void UFN_RandomOutputWithWeights::ExecuteInput(const FName& PinName)
+{
+	Super::ExecuteInput(PinName);
+
+	if (PinName == TEXT("In"))
+	{
+		ExecuteRandomOutput();
+		return;
+	}
+
+	if (PinName == TEXT("Reset Weights"))
+	{
+		for(auto& CurrentOutput : Outputs)
+		{
+			CurrentOutput.CurrentWeight = CurrentOutput.BaseWeight;
+		}
+	}
+}
---
+++ Plugins/FlowExtension/Source/FlowExtension/Public/Other/FN_RandomOutputWithWeights.h
@@ -0,0 +1,65 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Nodes/FlowNode.h"
+#include "FN_RandomOutputWithWeights.generated.h"
+
+USTRUCT(BlueprintType)
+struct FRandomOutputWithWeight
+{
+	GENERATED_BODY()
+
+	UPROPERTY(Category = "Randomness", EditAnywhere, BlueprintReadWrite)
+	FName OutputPinName;
+	
+	UPROPERTY(Category = "Randomness", EditAnywhere, BlueprintReadWrite)
+	float BaseWeight = 1;
+
+	/**How much is @BaseWeight reduced by when this
+	 * option is picked?*/
+	UPROPERTY(Category = "Randomness", EditAnywhere, BlueprintReadWrite)
+	float WeightLostWhenPicked = 0;
+
+	/**How much does the @BaseWeight get increased each
+	 * time the chance is rolled?
+	 * This is not added to the output that was picked.*/
+	UPROPERTY(Category = "Randomness", EditAnywhere, BlueprintReadWrite)
+	float WeightGainedPerRoll = 0;
+
+	UPROPERTY(Category = "Randomness", BlueprintReadWrite)
+	float CurrentWeight = 1;
+};
+
+UCLASS(DisplayName = "Random output with weights")
+class FLOWEXTENSION_API UFN_RandomOutputWithWeights : public UFlowNode
+{
+	GENERATED_UCLASS_BODY()
+	
+	UPROPERTY(Category = "Randomness", EditAnywhere, BlueprintReadWrite)
+	TArray<FRandomOutputWithWeight> Outputs;
+
+	/**Last chosen output, this is used to prevent the same output from being
+	 * chosen twice in a row.
+	 * Only kept track of if the outputs has 3 or more options.*/
+	UPROPERTY(Category = "Randomness", BlueprintReadWrite)
+	FRandomOutputWithWeight LastOutput;
+
+	UFUNCTION(Category = "Randomness", BlueprintCallable, meta = (ReturnDisplayName = "Chosen output"))
+	FRandomOutputWithWeight ExecuteRandomOutput();
+
+	
+#if WITH_EDITOR
+
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+
+private:
+
+	void RefreshOutputs();
+
+#endif
+
+protected:
+	virtual void ExecuteInput(const FName& PinName) override;
+};
