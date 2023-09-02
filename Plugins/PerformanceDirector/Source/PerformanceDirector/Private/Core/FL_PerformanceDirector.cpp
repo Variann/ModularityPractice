dc18c8671818d538cde4ec21393c0da33138f80b
Variann <kjartandth@gmail.com>
2023-09-02 07:28:41 +0200

    Added performance director function library

---
+++ Plugins/PerformanceDirector/Source/PerformanceDirector/Private/Core/FL_PerformanceDirector.cpp
@@ -0,0 +1,34 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+
+#include "Core/FL_PerformanceDirector.h"
+
+#include "Core/I_PerformanceDirector.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+TEnumAsByte<EPerformanceImportance> UFL_PerformanceDirector::GetActorsImportance(AActor* Actor)
+{
+	UAC_PerformanceDirector* PerformanceDirector;
+
+	//Interface retrieval is faster. If that fails, go through the actors components.
+	if(Actor->Implements<II_PerformanceDirector>())
+	{
+		PerformanceDirector = II_PerformanceDirector::Execute_GetPerformanceDirector(Actor);
+		if(!IsValid(PerformanceDirector))
+		{
+			UKismetSystemLibrary::PrintString(Actor, "Actor implements interface, but did not return GetPerformanceDirector");
+			return Normal;
+		}
+	}
+	else
+	{
+		PerformanceDirector = Cast<UAC_PerformanceDirector>(Actor->GetComponentByClass(UAC_PerformanceDirector::StaticClass()));
+		if(!IsValid(PerformanceDirector))
+		{
+			UKismetSystemLibrary::PrintString(Actor, "No PerformanceDirector component on actor, can't return importance.");
+			return Normal;
+		}
+	}
+	
+	return PerformanceDirector->GetCurrentImportance();
+}
---
+++ Plugins/PerformanceDirector/Source/PerformanceDirector/Public/Core/FL_PerformanceDirector.h
@@ -0,0 +1,20 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AC_PerformanceDirector.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "FL_PerformanceDirector.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PERFORMANCEDIRECTOR_API UFL_PerformanceDirector : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	UFUNCTION(Category = "Performance Director", BlueprintCallable)
+	static TEnumAsByte<EPerformanceImportance> GetActorsImportance(AActor* Actor);
+};
