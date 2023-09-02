c2adfbf89619f21a8b9e19682b2c0670efa3dafa
Variann <kjartandth@gmail.com>
2023-09-02 07:28:29 +0200

    Added performance director interface

---
+++ Plugins/PerformanceDirector/Source/PerformanceDirector/Private/Core/I_PerformanceDirector.cpp
@@ -0,0 +1,7 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+
+#include "Core/I_PerformanceDirector.h"
+
+
+// Add default functionality here for any II_PerformanceDirector functions that are not pure virtual.
---
+++ Plugins/PerformanceDirector/Source/PerformanceDirector/Public/Core/I_PerformanceDirector.h
@@ -0,0 +1,36 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AC_PerformanceDirector.h"
+#include "UObject/Interface.h"
+#include "I_PerformanceDirector.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UI_PerformanceDirector : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class PERFORMANCEDIRECTOR_API II_PerformanceDirector
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+
+	UFUNCTION(Category = "Performance Director", BlueprintCallable, BlueprintImplementableEvent)
+	UAC_PerformanceDirector* GetPerformanceDirector();
+
+	/**Figure out the current importance of this actor.*/
+	UFUNCTION(Category = "Performance Director", BlueprintCallable, BlueprintImplementableEvent)
+	TEnumAsByte<EPerformanceImportance> EvaluateImportance();
+
+	UFUNCTION(Category = "Performance Director", BlueprintCallable, BlueprintImplementableEvent)
+	void ImportanceUpdated();
+};
