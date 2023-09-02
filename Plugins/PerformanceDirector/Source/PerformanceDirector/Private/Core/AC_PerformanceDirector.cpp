ad2ac4a2d543aae5a4d2ee8aa09bd992a2026f9a
Variann <kjartandth@gmail.com>
2023-09-02 14:25:14 +0200

    AC_PerformanceDirector fully implemented

--- Plugins/PerformanceDirector/Source/PerformanceDirector/Private/Core/AC_PerformanceDirector.cpp
+++ Plugins/PerformanceDirector/Source/PerformanceDirector/Private/Core/AC_PerformanceDirector.cpp
@@ -3,15 +3,18 @@
 
 #include "Core/AC_PerformanceDirector.h"
 
+#include "Core/FL_PerformanceDirector.h"
+#include "Core/I_PerformanceDirector.h"
+#include "Kismet/GameplayStatics.h"
+
 
 // Sets default values for this component's properties
 UAC_PerformanceDirector::UAC_PerformanceDirector()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	PrimaryComponentTick.bCanEverTick = false;
+	bAutoActivate = false;
 }
 
 
@@ -19,18 +22,133 @@ UAC_PerformanceDirector::UAC_PerformanceDirector()
 void UAC_PerformanceDirector::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
 	
+	if(UKismetSystemLibrary::IsDedicatedServer(this))
+	{
+		if(!RunOnDedicatedServer)
+		{
+			DestroyComponent();
+			return;
+		}
+	}
+
+	if(StartTrackingOnBeginPlay)
+	{
+		StartTracking();
+	}
 }
 
-
-// Called every frame
-void UAC_PerformanceDirector::TickComponent(float DeltaTime, ELevelTick TickType,
-                                            FActorComponentTickFunction* ThisTickFunction)
+void UAC_PerformanceDirector::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	Super::EndPlay(EndPlayReason);
 
-	// ...
+	StopTracking(false);
 }
 
+void UAC_PerformanceDirector::StartTracking()
+{
+	if(TimerHandle.IsValid())
+	{
+		GetOwner()->GetWorldTimerManager().SetTimer(TimerHandle, this, &UAC_PerformanceDirector::StartTrackingTimer, UpdateInterval, true, 0);
+	}
+	else
+	{
+		UKismetSystemLibrary::PrintString(this, "Actor is already being tracked.");
+	}
+}
+
+void UAC_PerformanceDirector::StopTracking(bool bResetImportance)
+{
+	if(bResetImportance)
+	{
+		ResetImportance();
+	}
+	
+	TimerHandle.Invalidate();
+	CurrentImportance = DefaultImportance;
+}
+
+TEnumAsByte<EPerformanceImportance> UAC_PerformanceDirector::GetCurrentImportance(bool EvaluateImportance)
+{	
+	if(EvaluateImportance)
+	{
+		if(GetOwner()->GetClass()->ImplementsInterface(UI_PerformanceDirector::StaticClass()))
+		{
+			return II_PerformanceDirector::Execute_EvaluateImportance(GetOwner());
+		}
+		else
+		{
+			UKismetSystemLibrary::PrintString(this, "Tried to evaluate importance, but actor does not implement interface");
+		}
+	}
+
+	return CurrentImportance;
+}
+
+void UAC_PerformanceDirector::SetImportance(TEnumAsByte<EPerformanceImportance> NewImportance)
+{
+	if(CurrentImportance != NewImportance)
+	{
+		const EPerformanceImportance OldImportance = CurrentImportance;
+		CurrentImportance = NewImportance;
+
+		II_PerformanceDirector::Execute_ImportanceUpdated(GetOwner(), OldImportance, NewImportance);
+	}
+}
+
+void UAC_PerformanceDirector::ResetImportance()
+{
+	CurrentImportance = DefaultImportance;
+}
+
+void UAC_PerformanceDirector::SetUpdateInterval(float NewUpdateInterval)
+{
+	UpdateInterval = NewUpdateInterval;
+}
+
+void UAC_PerformanceDirector::StartTrackingTimer()
+{
+	/**Dirty hack to have a "multithreaded timer". I'm sure some programmers
+	 * with multithreading experience are screaming inside looking at this,
+	 * but hey... It works!*/
+	if(!bIsTracking)
+	{
+		bIsTracking = true;
+
+		/**Evaluate if we want background or game thread.
+		 * From my testing, all other ENamedThreads weren't worth
+		 * using, some didn't seem to work and some put the task
+		 * on the RHI thread. AnyThread was the only one who moved
+		 * the task to an unused thread.*/
+		ENamedThreads::Type NamedThread = ENamedThreads::AnyThread;
+		if(Thread == GameThread)
+		{
+			NamedThread = ENamedThreads::GameThread;
+		}
+		else if(Thread == BackgroundThread)
+		{
+			NamedThread = ENamedThreads::AnyThread;
+		}
+
+		//Async tasks seem to crash less if you use TWeakObjectPtr.
+		TWeakObjectPtr<UAC_PerformanceDirector> Director = this;
+		AsyncTask(NamedThread, [Director]()
+		{
+			if(Director.Get())
+			{
+				TEnumAsByte<EPerformanceImportance> LatestImportance = UFL_PerformanceDirector::GetActorsImportance(Director.Get()->GetOwner(), true);
+				AsyncTask(ENamedThreads::GameThread, [Director, LatestImportance]()
+				{
+					if(Director.Get())
+					{
+						if(Director.Get()->CurrentImportance != LatestImportance)
+						{
+							Director.Get()->SetImportance(LatestImportance);
+						}
+						Director.Get()->bIsTracking = false;
+					}
+				});
+			}
+		});
+	}
+}
--- Plugins/PerformanceDirector/Source/PerformanceDirector/Public/Core/AC_PerformanceDirector.h
+++ Plugins/PerformanceDirector/Source/PerformanceDirector/Public/Core/AC_PerformanceDirector.h
@@ -6,8 +6,45 @@
 #include "Components/ActorComponent.h"
 #include "AC_PerformanceDirector.generated.h"
 
+/**This enum is designed to be expanded
+ * or modified depending on what your team
+ * deems as a "importance level", though
+ * there should always be a "Unknown" in
+ * the case that an importance can't be
+ * calculated. No gameplay logic or updates
+ * should follow an Unknown importance update.*/
+UENUM(Blueprintable)
+enum EPerformanceImportance
+{
+	//Used for when the game thread needs to know NOW what the importance
+	//is, but the other thread has not finished the calculation.
+	//This should never be used for DefaultImportance.
+	Unknown,
+	//Hero objects typically should have no limits applied to them.
+	Hero,
+	Critical,
+	High,
+	//Actor should not try to modify its code or general behavior.
+	//Actor should behave as by default.
+	Normal,
+	Background,
+	Low
+};
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+UENUM(BlueprintType)
+enum EUpdateThread
+{
+	//Fastest and most responsive, but impacts
+	//game performance the most.
+	GameThread,
+	//Slowest and least responsive, but has
+	//extremely little impact on performance.
+	BackgroundThread
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FImportanceUpdated, TEnumAsByte<EPerformanceImportance>, OldImportance, TEnumAsByte<EPerformanceImportance>, NewImportance);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), DisplayName = "Performance Director")
 class PERFORMANCEDIRECTOR_API UAC_PerformanceDirector : public UActorComponent
 {
 	GENERATED_BODY()
@@ -16,12 +53,71 @@ public:
 	// Sets default values for this component's properties
 	UAC_PerformanceDirector();
 
+	/**Typically this component is automatically destroyed on
+	 * dedicated servers. This lets you disable that behavior.*/
+	UPROPERTY(Category = "Settings", BlueprintReadOnly, EditAnywhere)
+	bool RunOnDedicatedServer = false;
+
+	UPROPERTY(Category = "Settings", BlueprintReadOnly, EditAnywhere)
+	bool StartTrackingOnBeginPlay = false;
+
+	/**What importance should this actor have on BeginPlay or what
+	 * importance should be set when ResetImportance is called?*/
+	UPROPERTY(Category = "Settings", BlueprintReadWrite, EditAnywhere)
+	TEnumAsByte<EPerformanceImportance> DefaultImportance = Normal;
+
+	/**Generally not recommended to change during runtime, will
+	 * only go into affect after tracking is reset.
+	 *
+	 * Majority of the time you will want @BackgroundThread,
+	 * @GameThread should rarely be used.*/
+	UPROPERTY(Category = "Settings", BlueprintReadWrite, EditAnywhere)
+	TEnumAsByte<EUpdateThread> Thread = BackgroundThread;
+
+	/**How often should this actor be checked for updates?
+	 * If 0, it will check every frame.
+	 * TODO: If 0, use tick instead.*/
+	UPROPERTY(Category = "Settings", BlueprintReadOnly, EditAnywhere)
+	float UpdateInterval = 0.1;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	FImportanceUpdated ImportanceUpdated;
+
+	TQueue<EPerformanceImportance> ThreadTaskQueue;
+
 protected:
+
+	EPerformanceImportance CurrentImportance = Normal;
+
+	bool bIsTracking = false;
+
+	FTimerHandle TimerHandle;
+	
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	//Need to make sure to stop the thread when this component is destroyed.
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
 public:
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
-	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(Category = "Performance Director", BlueprintCallable)
+	void StartTracking();
+
+	UFUNCTION(Category = "Performance Director", BlueprintCallable)
+	void StopTracking(bool bResetImportance = true);
+
+	UFUNCTION(Category = "Performance Director", BlueprintCallable)
+	TEnumAsByte<EPerformanceImportance> GetCurrentImportance(bool EvaluateImportance);
+
+	UFUNCTION(Category = "Performance Director", BlueprintCallable)
+	void SetImportance(TEnumAsByte<EPerformanceImportance> NewImportance);
+
+	UFUNCTION(Category = "Performance Director", BlueprintCallable)
+	void ResetImportance();
+	
+	UFUNCTION(Category = "Performance Director", BlueprintCallable)
+	void SetUpdateInterval(float NewUpdateInterval);
+
+	void StartTrackingTimer();
 };
