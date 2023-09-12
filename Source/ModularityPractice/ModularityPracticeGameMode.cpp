b7db5b3683a11a8d74348b59d1a5bc47b1dbae1d
Variann <kjartandth@gmail.com>
2023-09-12 04:06:42 +0200

    Fix: Crash if player was referencing a flow asset

--- Source/ModularityPractice/ModularityPracticeGameMode.cpp
+++ Source/ModularityPractice/ModularityPracticeGameMode.cpp
@@ -6,10 +6,5 @@
 
 AModularityPracticeGameMode::AModularityPracticeGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+
 }
