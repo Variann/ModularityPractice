e6c7896587dcf8249b7416e8ab1c70d90e6584d8
Variann <kjartandth@gmail.com>
2023-12-04 16:34:30 +0100

    Added ambient dialogue data asset

---
+++ Plugins/AmbientDialogueManager/Source/AmbientDialogueManager/Private/Core/DA_AmbientDialogue.cpp
@@ -0,0 +1,44 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+
+#include "Core/DA_AmbientDialogue.h"
+
+#include "O_AmbientDialogueRequirement.h"
+#include "Core/AC_DialogueController.h"
+#include "Core/DialogueManager_SubSystem.h"
+#include "Kismet/GameplayStatics.h"
+
+bool UDA_AmbientDialogue::IsPlayable(AActor* Actor)
+{
+	UDialogueManager_SubSystem* DialogueManager = UGameplayStatics::GetPlayerController(this, 0)->GetLocalPlayer()->GetSubsystem<UDialogueManager_SubSystem>();
+	
+	if(DialogueManager->TrackedDialogue.Contains(this))
+	{
+		//Check if the dialogue has been played too recently
+		return false;
+	}
+	
+	UAC_DialogueController* DialogueController = Cast<UAC_DialogueController>(Actor->GetComponentByClass(UAC_DialogueController::StaticClass()));
+
+	if(DialogueController->CurrentDialogueAudio)
+	{
+		//Actor is playing dialogue, can't play
+		return false;
+	}
+	
+
+	return true;
+}
+
+bool UDA_AmbientDialogue::AreRequirementsMet(AActor* Actor)
+{
+	for(auto& CurrentRequirement : Requirements)
+	{
+		if(!CurrentRequirement->IsConditionMet(Actor))
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
---
+++ Plugins/AmbientDialogueManager/Source/AmbientDialogueManager/Public/Core/DA_AmbientDialogue.h
@@ -0,0 +1,50 @@
+﻿// Copyright (C) Varian Daemon 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DA_AmbientDialogue.generated.h"
+
+class UO_AmbientDialogueRequirement;
+
+UENUM(BlueprintType)
+enum EDialoguePriority
+{
+	Background,
+	Low,
+	Medium,
+	High,
+	Critical
+};
+
+UCLASS()
+class AMBIENTDIALOGUEMANAGER_API UDA_AmbientDialogue : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(Category = "Dialogue", BlueprintReadWrite, EditAnywhere)
+	TSoftObjectPtr<USoundBase> DialogueSound = nullptr;
+
+	UPROPERTY(Category = "Dialogue", BlueprintReadWrite, EditAnywhere)
+	TEnumAsByte<EDialoguePriority> Priority;
+
+	/**For this dialogue to be playable, all of these requirements must be met.*/
+	UPROPERTY(Category = "Dialogue", BlueprintReadWrite, EditAnywhere, Instanced)
+	TArray<UO_AmbientDialogueRequirement*> Requirements;
+
+	/**The minimum and maximum timer range for when this dialogue is playable again.*/
+	UPROPERTY(Category = "Dialogue", BlueprintReadWrite, EditAnywhere)
+	FVector2D TimerRange;
+
+	UPROPERTY(Category = "ADM", BlueprintReadWrite, EditAnywhere)
+	USoundAttenuation* SoundAttenuation = nullptr;
+
+	UFUNCTION(Category = "ADM", BlueprintCallable, BlueprintPure)
+	bool IsPlayable(AActor* Actor);
+
+	UFUNCTION(Category = "ADM", BlueprintCallable, BlueprintPure)
+	bool AreRequirementsMet(AActor* Actor);
+};
