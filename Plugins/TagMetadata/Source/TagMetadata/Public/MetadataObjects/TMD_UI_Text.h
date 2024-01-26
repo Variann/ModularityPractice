cfa1c634c05743cc4b2f1c5ab4cd20b97438ab24
Variann <kjartandth@gmail.com>
2024-01-26 15:16:04 +0100

    Added tags metadata example

--- Plugins/TagMetadata/Source/TagMetadata/Public/MetadataObjects/TMD_UI_Text.h
+++ Plugins/TagMetadata/Source/TagMetadata/Public/MetadataObjects/TMD_UI_Text.h
@@ -7,9 +7,12 @@
 #include "TMD_UI_Text.generated.h"
 
 /**
- * 
+ * Default example class for tag metadata.
+ * Simply create a child of UO_TagMetadata, add the Edit
+ * UPROPERTY specifier you want and set your UCLASS
+ * display name for an easier-to-read name in the editor.
  */
-UCLASS(DisplayName = "Tag UI Text")
+UCLASS(DisplayName = "UI Text")
 class TAGMETADATA_API UTMD_UI_Text : public UO_TagMetadata
 {
 	GENERATED_BODY()
